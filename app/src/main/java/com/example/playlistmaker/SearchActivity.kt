8b9e5bff4eb7b20b9ffee83d00dac695220b82fb
Daniil <daniil.kvasnikov@gmail.com>
2025-08-08 11:11:10 +0300

    save load text input

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -15,6 +15,7 @@ import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.internal.ViewUtils.hideKeyboard
 
 class SearchActivity : AppCompatActivity() {
+    private var stringInput : String = ""
     @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +39,7 @@ class SearchActivity : AppCompatActivity() {
         search.addTextChangedListener(object: TextWatcher {
             override fun afterTextChanged(s: Editable?) {
                 btnClear.visibility = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
+                stringInput = s.toString()
             }
 
             override fun beforeTextChanged(
@@ -59,5 +61,23 @@ class SearchActivity : AppCompatActivity() {
 
         }
         )
+    }
+
+    override fun onSaveInstanceState(
+        outState: Bundle
+    ) {
+        super.onSaveInstanceState(outState)
+        outState.putString(STRING_INPUT, stringInput)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        stringInput = savedInstanceState.getString(STRING_INPUT, "")
+        val search = findViewById<EditText>(R.id.edittext_serach)
+        search.setText(stringInput)
+    }
+
+    companion object {
+        const val STRING_INPUT = "STRING_INPUT"
     }
 }
