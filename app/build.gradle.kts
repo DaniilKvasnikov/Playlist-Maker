b4cccfadf16ba5634ad870e436daa64db4a18ca2
Daniil <daniil.kvasnikov@gmail.com>
2025-08-24 23:08:05 +0300

    implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
 }
 
 dependencies {
-    implementation("com.google.android.material:material:1.12.0")
+    implementation(libs.material)
     implementation("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
     implementation("com.google.code.gson:gson:2.13.1")
