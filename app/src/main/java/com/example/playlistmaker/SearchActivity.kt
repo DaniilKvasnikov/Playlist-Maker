f69936c4d1f632505a4e73e02d12b9054f978aec
Daniil <daniil.kvasnikov@gmail.com>
2025-08-24 15:08:10 +0300

    not found music

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -10,6 +10,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 import androidx.core.widget.doOnTextChanged
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -52,7 +53,9 @@ class SearchActivity : AppCompatActivity() {
             btnClear.visibility = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
             stringInput = s.toString()
         }
+        val emptyView = findViewById<View>(R.id.empty_placeholder)
         val recycleView = findViewById<RecyclerView>(R.id.recycler)
+
         val data = mutableListOf(
             Track("Smells Like Teen Spirit", "Nirvana", "5:01", "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"),
             Track("Billie Jean", "Michael Jackson", "4:35", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"),
@@ -60,6 +63,11 @@ class SearchActivity : AppCompatActivity() {
             Track("Whole Lotta Love", "Led Zeppelin", "5:33", "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"),
             Track("Sweet Child O'Mine", "Guns N' Roses", "5:03", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg")
         )
+        fun updateEmptyState() {
+            emptyView.isVisible = data.isEmpty()
+            recycleView.isVisible = data.isNotEmpty()
+        }
+        updateEmptyState()
         recycleView.layoutManager = LinearLayoutManager(this)
         val adapter = TrackAdapter(data)
         recycleView.adapter = adapter
@@ -85,9 +93,14 @@ class SearchActivity : AppCompatActivity() {
                             data.add(Track(trackName, artistName, duration, artwork))
                         }
                         adapter.notifyDataSetChanged()
+                        updateEmptyState()
                     }
 
+                    @SuppressLint("NotifyDataSetChanged")
                     override fun onFailure(call: retrofit2.Call<ResponseBody>, t: Throwable) {
+                        data.clear()
+                        adapter.notifyDataSetChanged()
+                        updateEmptyState()
                     }
 
                 })
