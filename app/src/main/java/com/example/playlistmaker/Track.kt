a0ee702b5678d2f54bef1c26600a9d21d208014d
Daniil <daniil.kvasnikov@gmail.com>
2025-09-04 21:16:14 +0300

    trackId

--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -4,6 +4,8 @@ import com.google.gson.annotations.SerializedName
 import java.util.Locale
 
 data class Track(
+    @SerializedName("trackId")
+    val trackId: Int,
     @SerializedName("trackName")
     val trackName: String,
     @SerializedName("artistName")
