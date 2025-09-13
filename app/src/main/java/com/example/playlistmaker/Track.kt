19dacf7dccfbb11aefe5319aab2e787c0898684c
Daniil <daniil.kvasnikov@gmail.com>
2025-09-13 21:10:11 +0300

    Serializable

--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -1,6 +1,7 @@
 package com.example.playlistmaker
 
 import com.google.gson.annotations.SerializedName
+import java.io.Serializable
 import java.util.Locale
 
 data class Track(
@@ -22,7 +23,7 @@ data class Track(
     val primaryGenreName: String,
     @SerializedName("country")
     val country: String
-) {
+) : Serializable {
     val trackTime: String
         get() = formatMillis(trackTimeMillis)
 
