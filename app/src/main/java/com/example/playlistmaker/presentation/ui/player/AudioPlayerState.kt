7932a40271e8475fef323fdc2a4ae90c0badcbd6
DaniilNotebook <123yk123@mail.ru>
2025-11-02 18:29:28 +0300

    sealed interface AudioPlayerState

--- app/src/main/java/com/example/playlistmaker/presentation/ui/player/AudioPlayerState.kt
+++ app/src/main/java/com/example/playlistmaker/presentation/ui/player/AudioPlayerState.kt
@@ -2,9 +2,9 @@ package com.example.playlistmaker.presentation.ui.player
 
 import com.example.playlistmaker.domain.models.Track
 
-sealed class AudioPlayerState {
-    data class Prepared(val track: Track) : AudioPlayerState()
-    data class Playing(val track: Track, val position: Int = 0) : AudioPlayerState()
-    data class Paused(val track: Track, val position: Int) : AudioPlayerState()
-    data class Completed(val track: Track) : AudioPlayerState()
+sealed interface AudioPlayerState {
+    data class Prepared(val track: Track) : AudioPlayerState
+    data class Playing(val track: Track, val position: Int = 0) : AudioPlayerState
+    data class Paused(val track: Track, val position: Int) : AudioPlayerState
+    data class Completed(val track: Track) : AudioPlayerState
 }
