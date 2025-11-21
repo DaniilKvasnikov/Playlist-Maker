4cfcf65a85b77ab8d21fa962dcb08f078617f6d1
Daniil <daniil.kvasnikov@gmail.com>
2025-11-21 12:44:43 +0300

    delete import unused

--- app/src/main/java/com/example/playlistmaker/search/ui/TrackAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/search/ui/TrackAdapter.kt
@@ -3,8 +3,6 @@ package com.example.playlistmaker.search.ui
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
