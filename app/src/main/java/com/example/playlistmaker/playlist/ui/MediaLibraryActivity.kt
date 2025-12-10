b75eb1e971d477c07d8e3fe107a4dc06d1be8dd9
Daniil <daniil.kvasnikov@gmail.com>
2025-12-11 01:59:49 +0300

    refactor import

--- app/src/main/java/com/example/playlistmaker/playlist/ui/MediaLibraryActivity.kt
+++ app/src/main/java/com/example/playlistmaker/playlist/ui/MediaLibraryActivity.kt
@@ -7,7 +7,6 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.ActivityMediaLibraryBinding
-import com.example.playlistmaker.playlist.ui.MediaLibraryViewPagerAdapter
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MediaLibraryActivity : AppCompatActivity() {
