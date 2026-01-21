f2733b87e5a8b1e1d0e9f9a215e81ef538edb8ab
Daniil <daniil.kvasnikov@gmail.com>
2026-01-21 23:16:37 +0300

    flow

--- app/src/main/java/com/example/playlistmaker/search/data/repository/TracksRepositoryImpl.kt
+++ app/src/main/java/com/example/playlistmaker/search/data/repository/TracksRepositoryImpl.kt
@@ -4,26 +4,28 @@ import com.example.playlistmaker.search.data.mapper.TrackMapper
 import com.example.playlistmaker.search.data.network.ITunesApiService
 import com.example.playlistmaker.search.domain.api.TracksRepository
 import com.example.playlistmaker.search.domain.models.Track
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class TracksRepositoryImpl(
     private val apiService: ITunesApiService,
     private val mapper: TrackMapper
 ) : TracksRepository {
 
-    override suspend fun searchTracks(query: String): Result<List<Track>> {
-        return try {
+    override fun searchTracks(query: String): Flow<Result<List<Track>>> = flow {
+        try {
             val response = apiService.getMusics(query)
             if (response.isSuccessful) {
                 val body = response.body()
                 val tracks = body?.results?.let {
                     mapper.mapDtoListToDomainList(it)
                 } ?: emptyList()
-                Result.success(tracks)
+                emit(Result.success(tracks))
             } else {
-                Result.failure(Exception("API Error: ${response.code()}"))
+                emit(Result.failure(Exception("API Error: ${response.code()}")))
             }
         } catch (e: Exception) {
-            Result.failure(e)
+            emit(Result.failure(e))
         }
     }
 }
--- app/src/main/java/com/example/playlistmaker/search/domain/api/SearchTracksUseCase.kt
+++ app/src/main/java/com/example/playlistmaker/search/domain/api/SearchTracksUseCase.kt
@@ -1,7 +1,8 @@
 package com.example.playlistmaker.search.domain.api
 
 import com.example.playlistmaker.search.domain.models.Track
+import kotlinx.coroutines.flow.Flow
 
 interface SearchTracksUseCase {
-    suspend operator fun invoke(query: String): Result<List<Track>>
+    operator fun invoke(query: String): Flow<Result<List<Track>>>
 }
